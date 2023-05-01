9c56ce42c7420156a058977d8225430f7f587942
Abhay 076 <yadavabhaycs@gmail.com>
2023-05-01 13:37:03 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,48 @@
 import RestrauntCard from "./RestrauntCard";
 import { useState } from "react";
 import restaurantList from "../utils/constants";
-const Body = () => {
+function filterData(searchText, listOfRestaurant) {
+  const filterData = listOfRestaurant.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
 
+const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
 
   // let listOfRestaurant = [
   //   ,
   // ];
   return (
     <div>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, listOfRestaurant);
+            setListOfRestaurant(data)
+          }}
+        >search</button>
+      </div>
       <div className="filter">
         <button
           className="filter_btn"
           onClick={() => {
-           let filteredList = listOfRestaurant.filter(
+            let filteredList = listOfRestaurant.filter(
               (res) => res.data.avgRating > 4
             );
             // console.log(listOfRestaurant)
-            setListOfRestaurant(filteredList)
+            setListOfRestaurant(filteredList);
           }}
         >
           Top Rated Restruants
